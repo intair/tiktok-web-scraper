4cef727067b86920c2382f39e075ae804414cd89
intair <contact@intair.me>
2022-01-10 21:58:47 +0000

    Updated/Fixed & Added Profile Links

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /**
  * @author intair
  * @since 12/23/2021
+ * @fixed 1/10/2021
  */
 
+// Replace USER-HERE with a Valid Username
+const user = 'USER-HERE'
+
 const axios = require('axios');
-
-const fs = require('fs')
-
-const { JSDOM }= require("jsdom");
 
 const getTikTokURL = (tiktok_username) => `https://www.tiktok.com/@${tiktok_username}`;
 
@@ -26,32 +26,31 @@ async function User(tiktok_username) {
 		}
 	});
 	
-	const parse = (str, l, r) => str.split(l)[1].split(r)[0];
+	const parse = (str, l, r) => str.split(l)[1]?.split(r)[0] || "N/A";
 
 	const ProfileUsername = parse(data, `"uniqueId":"`, `",`),
-		ProfilePFPURL = new JSDOM(data).window.document.querySelector("#main > div.jsx-834240835.main-body.page-with-header.middle.em-follow > div.jsx-2958430982.share-layout.compact.middle > div > header > div.share-info > div.jsx-260838931.image-wrap.big.user-page-header > span > img").getAttribute("src"),
 		ProfileName = parse(data, `"nickname":"`, `",`),
 		isVerified = parse(data, `"verified":`, `,`),
 		isPrivate = parse(data, `"privateAccount":`, `,`),
 		ProfileBio = parse(data, `signature":"`, `"`).replace(/\\n/g, "\n"),
+		ProfileLink = parse(data, `"tiktok-847r2g-SpanLink e1h20w7z2">`, `</span>`),
 		ProfileFollowing = parse(data, `"followingCount":`, `,`),
-		ProfileFollowers =  parse(data, `"Followers">`, `<`),
-		ProfileLikes = parse(data, `"Likes">`, `<`)
+		ProfileFollowers =  parse(data, `"followers-count">`, `</`),
+		ProfileLikes = parse(data, `"likes-count">`, `</`)
 
 console.log
 (`Username => @${ProfileUsername}
 Name= > ${ProfileName}
-Profile Pictue URL = > ${ProfilePFPURL}
 Verified? => ${isVerified}
 Private? => ${isPrivate}
-Bio => ${ProfileBio ? ProfileBio : "No Bio"}
+Bio => ${ProfileBio}
+Linked Website => ${ProfileLink}
 Following => ${ProfileFollowing}
 Followers => ${ProfileFollowers}
 Likes => ${ProfileLikes}`);
 
 	} catch (e) {
-		console.log('User not found!');
+		console.log('User Doesn\'t Exist.')
 	}
 }
-
-User('USER-HERE')
+User(user)
